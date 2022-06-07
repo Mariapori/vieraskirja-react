90e82bd33b675158e1ce47c565b58b24e3a770d7
Topias Mariapori <topias@mariapori.fi>
2022-06-08 00:15:03 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+  const [kirjoitukset,setKirjoitukset] = useState([])
+
+  useEffect(() => {
+    fetch("https://api.mariapori.fi/Vieraskirja")
+    .then((response) => response.json())
+    .then((data) => setKirjoitukset(data))
+    .catch((error) => alert("Dataa ei voitu hakea."))
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Vieraskirja</h1>
+      <table className='table table-dark'>
+        <thead>
+        <tr>
+          <th>PVM</th>
+          <th>Kirjoittaja</th>
+          <th>Viesti</th>
+        </tr>
+        </thead>
+        <tbody>
+          {kirjoitukset.map((kirjoitus)=>
+          <tr key={kirjoitus.Id}>
+            <td>
+              {kirjoitus.PVM}
+            </td>
+            <td>
+              {kirjoitus.Kirjoittaja}
+            </td>
+            <td>
+              {kirjoitus.Viesti}
+            </td>
+          </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
